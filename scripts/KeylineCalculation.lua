679c835fb2ebec547580221c6a310933e2ac2cbf
Timmeey86 <Timmeey86@users.noreply.github.com>
2025-10-02 20:18:39 +0200

    increase line visibility

--- scripts/KeylineCalculation.lua
+++ scripts/KeylineCalculation.lua
@@ -139,7 +139,7 @@ function KeylineCalculation.drawLines(keylines, exportedKeylines, importedParall
 			if y1 == nil or y2 == nil then
 				break
 			end
-			DebugUtil.drawDebugLine(x1, y1 + 1, z1, x2, y2 + 1, z2, color[1], color[2], color[3], 0)
+			DebugUtil.drawDebugLine(x1, y1 + 1, z1, x2, y2 + 1, z2, color[1], color[2], color[3], .5)
 
 			--Utils.renderTextAtWorldPosition(x1, y1 + .4, z1 , string.format("%d", i-1), getCorrectTextSize(0.02), 0,color[1], color[2], color[3], 1)
 
